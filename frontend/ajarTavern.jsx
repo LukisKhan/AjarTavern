5766ca014e0ab9319d98725cc015f2d000641b15
Luat Pham <luat32@gmail.com>
2019-09-02 18:25:15 -0500

    Added session reducer/actions/utilRequest

---
+++ frontend/ajarTavern.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import createStore from './store/store';
+// import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  // ReactDOM.render(root);
+})
