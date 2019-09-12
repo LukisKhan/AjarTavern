7b3b2e9a0d5f7bc9747bfa57e4201d1c35f9ecbc
Luat Pham <luat32@gmail.com>
2019-09-12 12:51:46 -0500

    Fixed seed file to attach photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+User.destroy_all
+Restaurant.destroy_all
 
 User.create(firstname: "tommy", lastname: "deuk", email: "tommy@looksgoodto.me", password: "hunter12");
 User.create(firstname: "andy", lastname: "wynhoop", email: "andy@looksgoodto.me", password: "hunter12");
 User.create(firstname: "luke", lastname: "pham", email: "luke@looksgoodto.me", password: "hunter12");
 
-Restaurant.create(name:"Pho Doan", address:"123 st", phone_num:"555-555", lat: 1.111, lng: 1.222);
+rest1 = Restaurant.create(name:"Pho Doan", address:"123 st", phone_num:"555-555", lat: 1.111, lng: 1.222);
+file = open('https://full-stacks-lp-aa-dev.s3.us-east-2.amazonaws.com/dong_que.jpeg')
+rest1.photo.attach(io: file, filename: "restpic")
